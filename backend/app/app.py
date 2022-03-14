5d500934ed30a15013f2b358b36ec34ab2c6fd91
maccam91 <michael.cortezjr@gmail.com>
2022-03-14 14:35:45 -0500

    updating structure on files and folders

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,45 +1,54 @@
-from flask import Flask, request
-# from resources.models import class_api
-from resources.user import User,Content, Post
+import os
+from flask import Flask, request, render_template
+
 from flask_mongoengine import MongoEngine
+from flask_mongoengine.wtf import model_form
+from wtforms import validators
+from dotenv import load_dotenv
 from flask_cors import CORS
 
+load_dotenv()
+URI = os.getenv("MONGO_URI")
+
 app = Flask(__name__, static_folder='E:/sei1213/unit4/CAPSTONE/CAPSTONE/frontend', static_url_path='')
-# app.register_blueprint(class_api, url_prefix='/api/v1')
-app.config['MONGODB_SETTINGS']= {
-    'db': 'FIRESIDE',
-    'host':'127.0.0.1',
-    'port':27017
-}
-db = MongoEngine(app)
+
+app.config['MONGODB_SETTINGS']= {URI or "mongodb://127.0.0.1:27017/FIRESIDE"} 
+# db = MongoEngine(app)
+# db.init_app(app)
 app.config['MONGODB_CONNECT'] = False
-@app.route('/posttest', methods=['POST', 'GET', 'DELETE', 'PUT'])
-def methodtest():
-    return f'this the method {request.method}'
+
+
+# Routes
+from user import routes
+# @app.route('/posttest', methods=['POST', 'GET', 'DELETE', 'PUT'])
+# def test():
+#     return render_template('home.html')
+
 @app.route('/')
 def homePage():
     return {"Hello":["world","my name is", "Michael"]}
-@app.route('/user/signup')
-
-@app.route('/classes')
-def classPage():
-    return {"Class":["This is the classes page"]}
 
 
-@app.route('/classes/create')
-def createClass():
-    return '<h1>Create a Class Here</h1>'
+
+# @app.route('/classes')
+# def classPage():
+#     return {"Class":["This is the classes page"]}
 
 
-@app.route('/monsters')
-def monsterPage():
-    return {"Monster": ["this is the monster page"]}
+# @app.route('/classes/create')
+# def createClass():
+#     return '<h1>Create a Class Here</h1>'
 
 
-@app.route('/monsters/create', methods=['POST', 'GET'])
-def createMonster():
-    print(request.get_json())
-    return "<h1>Create a Monster Here</h1>"
+# @app.route('/monsters')
+# def monsterPage():
+#     return {"Monster": ["this is the monster page"]}
+
+
+# @app.route('/monsters/create', methods=['POST', 'GET'])
+# def createMonster():
+#     print(request.get_json())
+#     return "<h1>Create a Monster Here</h1>"
 
 
 # @app.route('/items')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import FrontTest from './Component/test';
+import HomePage from './Component/Homepage';
+import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <FrontTest/>
+      <nav className='topNav'>
+        <Link to={'/'} element={<FrontTest/>}>Test</Link>
+        <Link to={'/signup'} element={<FrontTest/>}>Signup</Link>
+      </nav>
+      <Routes>
+      <Route path='/' element={<FrontTest/>}/>
+      <Route path='/signup' element={<HomePage/>}/>
+      </Routes>
     </div>
   );
 }
